aa28345c83d949b5248981cb9b22a6e785c8b968
kadonomaro <denis.afer@gmail.com>
2020-02-16 20:50:15 +0700

    fix: change logic

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navContainer = document.querySelector('.nav');
-    const navItems = navContainer.querySelectorAll('.nav__item');
-    const navList = navContainer.querySelector('.nav__list');
-    let oldChild = null;
-    let navItemsWidth = 0;
+    if (document.documentElement.clientWidth < 1024) {
+        const navContainer = document.querySelector('.nav');
+        const navList = navContainer.querySelector('.nav__list');
+        const navItems = navContainer.querySelectorAll('.nav__list .nav__item');
 
-    let subItem = document.createElement('li');
-    let subList = document.createElement('ul');
-    subItem.classList.add('nav__item', 'more');
-    subList.classList.add('sub-menu');
-    
-    let navContainerWidth = outerWidth(navContainer);
+        let oldChild = null;
+        let navItemsWidth = 0;
 
-    [...navItems].forEach((item) => {
-        navItemsWidth += outerWidth(item);
-    });
+        let subItem = document.createElement('li');
+        let subList = document.createElement('ul');
+        subItem.classList.add('nav__item', 'more');
+        subList.classList.add('sub-menu');
+        
+        let navContainerWidth = outerWidth(navContainer);
+
+        [...navItems].forEach((item) => {
+            navItemsWidth += outerWidth(item);
+        });
 
 
-    responsiveMenu();
-
-    window.addEventListener('resize', function () {
         responsiveMenu();
-    });
 
-    function responsiveMenu() {
+        window.addEventListener('resize', function () {
+            responsiveMenu();
+        });
 
-        navContainerWidth = outerWidth(navContainer);
+        function responsiveMenu() {
+            navContainerWidth = outerWidth(navContainer);
 
-        while (navItemsWidth > navContainerWidth) {
-            navItemsWidth -= outerWidth(navList.lastElementChild);
-            oldChild = navList.removeChild(navList.children[navList.children.length - 1]);
-            subList.insertBefore(oldChild, subList.firstChild);
+            while (navItemsWidth > navContainerWidth) {
+                navItemsWidth -= outerWidth(navList.lastElementChild);
+                console.dir(navList.children);
+                console.log(navList.children[position]);
+                if (navList.children[position]) {
+                    oldChild = navList.removeChild(navList.children[navList.children.length - 1]);
+                    subList.insertBefore(oldChild, subList.firstChild);
+                }
+            }
 
+            if (subList.children.length) {
+                subItem.innerHTML = '<a href="javascript:void(0)">...</a>'
+                subItem.appendChild(subList);
+                navList.appendChild(subItem);
+            }
         }
-        
-
-        if (subList.children.length) {
-            
-            subItem.appendChild(subList);
-            navList.appendChild(subItem);
-
-   
-        }
-        
-        
     }
 });
 
